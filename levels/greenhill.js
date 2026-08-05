c9837357d5ee146139978a0ec6934c63b5757ad3
Hedge <hedgeplays11@outlook.com>
2026-08-05 10:09:47 -0400

    Define greenHill level with platforms and goal

--- levels/greenhill.js
+++ levels/greenhill.js
@@ -1,1 +1,62 @@
+const greenHill = {
 
+    width: 6000,
+
+    platforms: [
+
+        // starting ground
+        {
+            x:0,
+            y:450,
+            width:800,
+            height:100
+        },
+
+        // first hill
+        {
+            x:850,
+            y:400,
+            width:500,
+            height:150
+        },
+
+        // floating platforms
+        {
+            x:1450,
+            y:350,
+            width:250,
+            height:40
+        },
+
+        {
+            x:1850,
+            y:300,
+            width:300,
+            height:40
+        },
+
+        // lower ground
+        {
+            x:2300,
+            y:450,
+            width:1000,
+            height:100
+        },
+
+        // final stretch
+        {
+            x:3500,
+            y:380,
+            width:900,
+            height:170
+        }
+
+    ],
+
+
+    goal: {
+        x:5600,
+        y:330
+    }
+
+};

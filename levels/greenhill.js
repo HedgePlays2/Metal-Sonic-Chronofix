e876b110950396c0a5fbbab3140baeea4674e4b3
Hedge <hedgeplays11@outlook.com>
2026-08-05 11:16:21 -0400

    Refactor platform definitions in greenhill.js

--- levels/greenhill.js
+++ levels/greenhill.js
@@ -1,62 +1,24 @@
 const greenHill = {
+    spawn: {
+        x: 150,
+        y: 300
+    },
 
     width: 6000,
 
     platforms: [
 
-        // starting ground
-        {
-            x:0,
-            y:450,
-            width:800,
-            height:100
-        },
-
-        // first hill
-        {
-            x:850,
-            y:400,
-            width:500,
-            height:150
-        },
-
-        // floating platforms
-        {
-            x:1450,
-            y:350,
-            width:250,
-            height:40
-        },
-
-        {
-            x:1850,
-            y:300,
-            width:300,
-            height:40
-        },
-
-        // lower ground
-        {
-            x:2300,
-            y:450,
-            width:1000,
-            height:100
-        },
-
-        // final stretch
-        {
-            x:3500,
-            y:380,
-            width:900,
-            height:170
-        }
+        { x: 0,    y: 450, w: 800,  h: 100 },
+        { x: 900,  y: 400, w: 350,  h: 150 },
+        { x: 1400, y: 350, w: 250,  h: 40  },
+        { x: 1800, y: 300, w: 300,  h: 40  },
+        { x: 2300, y: 450, w: 1000, h: 100 },
+        { x: 3500, y: 380, w: 800,  h: 170 }
 
     ],
 
-
     goal: {
-        x:5600,
-        y:330
+        x: 5600,
+        y: 330
     }
-
 };
